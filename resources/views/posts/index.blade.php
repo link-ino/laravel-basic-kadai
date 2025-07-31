7cd3c24496eeee3ef70cb5d0fa8059d813092978
link-ino <t-ino@link-si.co.jp>
2025-07-31 20:02:23 +0900

    update
    aa

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,5 +6,23 @@
 </head>
 <body>
     <h1>投稿一覧</h1>
+
+    <table>
+        <thead>
+            <tr>
+                <th>タイトル</th>
+                <th>本文</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($posts as $post)
+                <tr>
+                    <td>{{ $post->title }}</td>
+                    <td>{{ $post->content }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
 </body>
 </html>
